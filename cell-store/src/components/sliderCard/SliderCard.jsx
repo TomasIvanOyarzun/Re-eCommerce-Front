404b7332e20f6963bf6d8b26fa7586319f6d02c1
Tomas Ivan Oyarzun <65320107+TomasIvanOyarzun@users.noreply.github.com>
2022-10-20 07:39:35 -0300

    Update SliderCard.jsx

--- cell-store/src/components/sliderCard/SliderCard.jsx
+++ cell-store/src/components/sliderCard/SliderCard.jsx
@@ -4,25 +4,7 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { useSelector } from 'react-redux';
 import style from './SliderCard.module.css'
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
-};
+
 
 const SliderCard = () => {
  
@@ -38,18 +20,18 @@ const SliderCard = () => {
     <div className={style.container}>
      <div className={style.sub_container}>
     <h2>Celulares Samsung</h2>
-    <Carousel responsive={responsive} infinite={true} >
+    <Carousel  infinite={true} >
   <div>hola</div>
     
   </Carousel>
   <h2>Celulares Motorola</h2>
-  <Carousel responsive={responsive} infinite={true} >
+  <Carousel  infinite={true} >
      <div>hola</div>
     
   </Carousel>
 
   <h2>Celulares Iphone</h2>
-  <Carousel responsive={responsive}  >
+  <Carousel   >
    <div>hola</div>
     
      </Carousel>
