90997d86013b4108c116a250214e5670137228a6
Tomas Ivan Oyarzun <65320107+TomasIvanOyarzun@users.noreply.github.com>
2022-10-20 08:08:34 -0300

    Update CardPagination.jsx

--- cell-store/src/page/CardPagination/CardPagination.jsx
+++ cell-store/src/page/CardPagination/CardPagination.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Cards from '../../components/cards-products/Cards'
 import Paginado from '../../components/Paginado/Paginado'
+import SliderCard from '../../components/sliderCard/SliderCard'
 
 const CardPagination = () => {
   return (
